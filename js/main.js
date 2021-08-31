92feeae04a5528375f51735f2b1233258f5d4c2c
toufiq007 <t.limon2342@gmail.com>
2021-08-31 14:14:55 +0600

    complete but not validate

--- js/main.js
+++ js/main.js
@@ -7,13 +7,46 @@ const searchClubInfo = ()=>{
     
     fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${inputClubValue}`)
     .then(res=> res.json())
-    .then(data=> showClubInfo(data.teams[0]))
+    .then(data=> {
+      showClubInfo(data.teams[0]);
+      showModal(data.teams[0]);
+    })
 
     inputClub.value = ''
   })
 }
-
 searchClubInfo()
+
+let showModal = (club)=>{
+  let modalSection = document.getElementById("modal-section")
+  modalSection.innerHTML = `
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Club's full information</h5>
+              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <h5>Stadium: ${club.strStadium}</h5><img src="${club.strStadiumThumb}" class="img-fluid">
+              <h5 class="card-title">League: ${club.strSport}</h5>
+              <h5>Club Name:  ${club.strTeam}</h5>
+              <h5>Formated year: ${club.intFormedYear}</h5>
+              <h5>league: ${club.strLeague}</h5>
+              <h5>Stadium-location: ${club.strStadiumLocation}</h5>
+              <h5>Stadium-Capacity: ${club.intStadiumCapacity}</h5>
+              <h5>Joursey: </h5><img src="${club.strTeamJersey}" class="img-fluid">
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+    </div>
+  `
+}
 
 let showClubInfo = (clubs)=>{
   let clubInfo = document.getElementById("sportsCentre");
@@ -28,17 +61,10 @@ let showClubInfo = (clubs)=>{
           <h5>Team short: ${clubs.strTeamShort}</h5>
           <h5>league: ${clubs.strLeague}</h5>
           <h5>Stadium: ${clubs.strStadium}</h5>
-          <button type="button" id="btn" class="btn btn-primary" onclick="showModal('paris')" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Info</button>
+          <button type="button" id="btn" class="btn btn-primary" onclick="showModal()" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Info</button>
         </div>
       </div>
   `
   // showModal(clubs)
 }
 
-let showModal = (club)=>{
-  console.log(club)
-  let btn = document.getElementById("btn");
-  // btn.addEventListener("click",()=>{
-  //   console.log(club)
-  // })
-}
